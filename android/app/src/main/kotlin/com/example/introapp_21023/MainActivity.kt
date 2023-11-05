a91cb3e618eba942c9eb86d7466076a86038aa41
s.eslm <s.eslm@seslms-MacBook-Pro.local>
2023-11-05 17:09:21 +0300

    2.11.23

---
+++ android/app/src/main/kotlin/com/example/introapp_21023/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.introapp_21023
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/questions.dart
@@ -0,0 +1,19 @@
+import 'package:introapp_21023/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+      "Flutter'da bir widget'in boyutunu belirlemek için hangi widget'i kullanırsınız?",
+      ["A) Container", "B) Text", "C) Column", "D) Scaffold"]),
+  QuizQuestion(
+      "Flutter'da bir butonun tıklanma olayını yakalamak için hangi özellik kullanılır?",
+      ["A) onTap", "B) onPressed", "C) onButtonPress", "D) onClick"]),
+  QuizQuestion(
+      "Bir Flutter uygulamasında, birden fazla sayfayı yönetmek ve gezinmek için kullanılan sınıf veya widget hangisidir?",
+      ["A) MaterialApp", "B) Text", "C) Center", "D) Padding"]),
+  QuizQuestion(
+      "Flutter'da bir dizeyi ekranda görüntülemek için hangi widget kullanılır?",
+      ["A) StringText", "B) Text", "C) DisplayString", "D) ShowText"]),
+  QuizQuestion(
+      "Flutter'da bir widget'in rengini belirlemek için hangi özellik kullanılır??",
+      ["A) color", "B) style", "C) background", "D)  textColor"]),
+];
